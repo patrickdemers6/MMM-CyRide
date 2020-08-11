323984f02961c23e594408220e6b61c6e4a32d9b
Demers, Patrick <patrickdemers6@gmail.com>
2020-08-11 16:41:54 -0500

    put getColor in MMM-CyRide file

--- MMM-CyRide.js
+++ MMM-CyRide.js
@@ -1,4 +1,3 @@
-const getColor = require("./getColor");
 Module.register("MMM-CyRide", {
   defaults: { stopID: "5108903", customerID: "187" },
   start: function () {
@@ -67,3 +66,50 @@ Module.register("MMM-CyRide", {
     this.updateDom();
   }
 });
+const getColor = (routeName) => {
+  let color;
+  switch (routeName.split(" ")[0]) {
+    case "1":
+      color = "red";
+      break;
+    case "2":
+      color = "green";
+      break;
+    case "3":
+      color = "blue";
+      break;
+    case "5":
+      color = "yellow";
+      break;
+    case "6":
+      color = "brown";
+      break;
+    case "7":
+      color = "#8B008B"; // purple
+      break;
+    case "9":
+      color = "#DDA0DD"; // plum
+      break;
+    case "11":
+      color = "#7d161a"; // cherry
+      break;
+    case "12":
+      color = "#C8A2C8"; // lilac
+      break;
+    case "14":
+      color = "#FFE4C4"; // peach
+      break;
+    case "21":
+      color = "#C8102E"; // ISU CARDINAL -- GO CLONES!!
+      break;
+    case "23":
+      color = "orange";
+      break;
+    case "25":
+      color = "#F1BE48"; // ISU GOLD -- GO CLONES!!
+      break;
+    default:
+      color = "black"; // show nothing
+  }
+  return color;
+};
